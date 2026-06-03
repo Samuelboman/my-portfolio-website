2ee17a9ba9a44dfe8e2a36fdd77d9f5dd4dd50bb
samuelboman <samuelboman10@gmail.com>
2026-06-03 08:38:10 +0100

    fixed vercel deplloyment and profile picture

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import projectRoutes from "./routes/project.js";
 import contactRoutes from "./routes/contact.js";
 import aboutRoutes from "./routes/about.js";
 import homeRoutes from "./routes/home.js";
-import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const port = 5000;
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.set('views', './views');
+app.use(express.static(join(__dirname, 'public')));
+app.set('views', join(__dirname, 'views'));
 
 app.use("/", homeRoutes);
 app.use("/about", aboutRoutes);
@@ -20,6 +25,10 @@ app.use((req, res) => {
   res.status(404).render("404", { title: "404-Page" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+
+export default app;
